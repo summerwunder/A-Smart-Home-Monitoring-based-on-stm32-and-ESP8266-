fdc380db97cf0b8d26eb4c4647f67fc23fea63e3
wmr <mingrui@whut.edu.cn>
2023-10-22 23:06:17 +0800

    update

--- User/main.c
+++ User/main.c
@@ -8,6 +8,7 @@
 #include "ADC.h"
 #include "PWM.h"
 #include "ESP8266.h"
+#include "BUZZER.h"
 
 static void DHT11_Show(void);
 static void Servo_test(void);
@@ -22,6 +23,7 @@ int main(void)
     Key_Init();
     USART1_Init();
     ADC1_Init();
+    Buzzer_Init();
     DHT11_GPIO_Config();
     //PWM_Init();
     ESP8266_Init();
@@ -30,7 +32,7 @@ int main(void)
     memset(RxData,0,200);         //将接收区清0
     while (1)
     {                   	
-        if(RxFlag==1)
+        /*if(RxFlag==1)
         {
             if(strstr(RxData,"LED_ON"))
             {
@@ -44,14 +46,17 @@ int main(void)
             memset(RxData,0,200);         //将接收区清0
             RxFlag=0;
         }
-       
+        */
         DHT11_Show();
+        /*
         //ADC1_Value=ADC1_GetValue();
         //OLED_ShowNum(3,1,ADC1_Value,4);
         ESP8266_SendData(DHT11_Data.humi_int,DHT11_Data.temp_int); 
         //Servo_test();
         //printf("humi:%d temp:%d\r\n",DHT11_Data.humi_int,DHT11_Data.temp_int);
         //Delay_ms(400);        
+    }*/
+    Buzzer_run(1);
     }
 }
 
